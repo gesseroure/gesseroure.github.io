155939aff1bbb1ac2dc7b950ab5000459fe446e2
gesseroure <100392725+gesseroure@users.noreply.github.com>
2024-10-14 17:33:21 -0500

    Add files via upload

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,34 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1728945058',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/research/',
+      
+        '/publications/',
+      
+        '/about/',
+      
+        '/categories/',
+      
+        '/archives/',
+      
+        '/tags/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
